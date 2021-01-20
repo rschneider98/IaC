2a7c2deb90ee85bf331a6fed64170c960f847dbe
Richard <rschneider1st@gmail.com>
2021-01-20 16:35:12 -0600

    fixed jupytext and jupyter interaction

--- python_setup/linux_get_packages.sh
+++ python_setup/linux_get_packages.sh
@@ -3,10 +3,19 @@
 apt update && apt upgrade -y
 sudo apt-get install python3.7
 python3.7 -m pip install -r base-pacakges.txt
+
+# set up jupyter notebooks for data analysis 
+python3.7 -m jupyter notebook --generate-config
+echo "Set Jupyter Notebooks Password:"
+jupyter notebook password
+python3.7 -m jupyter nbextension install --py jupytext --user
+python3.7 -m jupyter serverextension enable jupytext
+
+# now create a python kernel and attach to notebook 
 cd ../..
 virtualenv --python=/usr/bin/python3.7 nb_env
 source nb_env/bin/activate
-python -m pip install -r nb-env-pkgs.txt
+python -m pip install -r IaC/python_setup/nb-env-pkgs.txt
 python -m ipykernel install --user --name=nb_env
 source nb_env/bin/deactivate
 cd IaC/python_setup
